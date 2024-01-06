c4bd037810b17ed342ebd1307b1dd11884849160
BlasterAlex <pichulev23071999@gmail.com>
2024-01-06 20:47:47 +0400

    Added support for multiple opponents without simulation

--- board.py
+++ board.py
@@ -13,15 +13,17 @@ import utils as ut
 class Board:
     """Store information on possible opponent hands."""
 
-    def __init__(self, combination: Tuple[str, ...]) -> None:
+    def __init__(self, combination: Tuple[str, ...], players: int = 2) -> None:
         """Generate initial opponent hands."""
         self._our_fcombination = cb.combination_to_fcombination(combination)
         # Generate the opponent fcombinations
-        self._opponent_fcombinations = self._generate_opponent_fcombinations()
+        self._central_fcombinations = self._generate_fcombinations(players)
+        self._opponents_fcombinations = [self.get_central_fcombinations() for _ in range(1, players)]
 
-    def _generate_opponent_fcombinations(self) -> List[Tuple[int, ...]]:
+    def _generate_fcombinations(self, players: int = 2) -> List[Tuple[int, ...]]:
         """Generate all the possible fcombinations of the opponent."""
-        raw_fcombinations = list(itertools.combinations(list(i for i in range(20)), 5))
+        positions = 5 if players < 4 else 4
+        raw_fcombinations = list(itertools.combinations(list(i for i in range(20)), positions))
         filtered_fcombinations = []
         our_fives = self._our_fcombination.count(10) + self._our_fcombination.count(11)
         for fcombination in raw_fcombinations:
@@ -45,20 +47,87 @@ class Board:
         return [fcombination for fcombination in fcombinations
                 if ut.HINTS[hint]['function'](fcombination) == answer]
 
-    def get_opponent_fcombinations(self) -> List[Tuple[int, ...]]:
-        """Return the possible fcombinations of the opponent."""
-        return copy.deepcopy(self._opponent_fcombinations)
+    def _filter_conflict_combinations(self,
+                                      fcombinations: List[Tuple[int, ...]],
+                                      target_fcombinations: List[Tuple[int, ...]]) -> List[Tuple[int, ...]]:
+        """Return the filtered fcombinations."""
+        if len(target_fcombinations) == 0:
+            return fcombinations
+        
+        matched_tiles = set.intersection(*[set(fcomb) for fcomb in target_fcombinations])
+        if len(matched_tiles) == 0:
+            return fcombinations
+        
+        filtered_fcombinations = []            
+        for fcombination in fcombinations:
+            ok = True
+            for matched_tile in matched_tiles:
+                if matched_tile in fcombination:
+                    ok = False
+                    break
+            if ok:
+                filtered_fcombinations.append(tuple(fcombination))
+        
+        return filtered_fcombinations
 
-    def apply_hint(self, hint: str, answer: int | str | List[str]) -> None:
+    def _try_to_apply_combination(self,
+                                  combination: Tuple[int, ...],
+                                  opponent: int = 0) -> bool:
+        
+        other_opponent_numbers = [num for num in range(len(self._opponents_fcombinations)) if num != opponent]
+        other_opponent_combinations = [self.get_opponent_fcombinations(opponent_num) for opponent_num in other_opponent_numbers]
+        filtered_opponent_combinations = [self._filter_conflict_combinations(opponent_combinations, [combination]) for opponent_combinations in other_opponent_combinations]
+
+        if len(filtered_opponent_combinations) < 2:
+            possible_opponent_combinations = filtered_opponent_combinations[0]
+        else:
+            possible_opponent_combinations = self._filter_conflict_combinations(filtered_opponent_combinations[0], filtered_opponent_combinations[1])
+        
+        return len(possible_opponent_combinations) > 0
+
+    def get_central_fcombinations(self) -> List[Tuple[int, ...]]:
+        """Return the possible central fcombinations."""
+        return copy.deepcopy(self._central_fcombinations)
+    
+    def get_opponents_fcombinations(self) -> List[List[Tuple[int, ...]]]:
+        """Return the possible opponents fcombinations."""
+        return copy.deepcopy(self._opponents_fcombinations)
+
+    def get_opponent_fcombinations(self, opponent: int = 0) -> List[Tuple[int, ...]]:
+        """Return the possible fcombinations of the opponent."""
+        return copy.deepcopy(self._opponents_fcombinations[opponent])
+
+    def apply_hint(self, hint: str, answer: int | str | List[str], opponent: int = 0) -> None:
         """Apply a hint on the current board state."""
-        self._opponent_fcombinations = self._filter_combinations(self._opponent_fcombinations,
-                                                                 hint,
-                                                                 answer)
+        opponent_fcombinations = self._filter_combinations(self._opponents_fcombinations[opponent],
+                                                           hint,
+                                                           answer)
+
+        if len(self._opponents_fcombinations) > 1:
+            filtered_fcombinations = []
+            for opponent_fcombination in opponent_fcombinations:
+                if self._try_to_apply_combination(opponent_fcombination, opponent):
+                    filtered_fcombinations.append(opponent_fcombination)
+            opponent_fcombinations = filtered_fcombinations
+
+        for index, fcombinations in enumerate(self._opponents_fcombinations):
+            if index == opponent:
+                self._opponents_fcombinations[index] = opponent_fcombinations
+            else:
+                self._opponents_fcombinations[index] = self._filter_conflict_combinations(fcombinations,
+                                                                                          opponent_fcombinations)
+
+        if len(self._opponents_fcombinations) == 1:
+            self._central_fcombinations = opponent_fcombinations
+        else:
+            for fcombinations in self._opponents_fcombinations:
+                self._central_fcombinations = self._filter_conflict_combinations(self._central_fcombinations,
+                                                                                fcombinations)
 
     def simulate(self, hint: str) -> Tuple[float, float]:
         """Return the average % of filtered combinations, and the standard deviation."""
         
-        opponent_fcombinations = self.get_opponent_fcombinations()
+        opponent_fcombinations = self.get_central_fcombinations()
         answers = [ut.HINTS[hint]['function'](fcombination) for fcombination in opponent_fcombinations]
         answers_count = {answer:answers.count(answer) for answer in answers}
 
--- main.py
+++ main.py
@@ -18,25 +18,35 @@ import combination as cb
 import menu as mn
 
 
-combination = mn.ask_user_combination()
+players = mn.ask_number_of_players()
+combination = mn.ask_user_combination(players)
 fcombination = cb.combination_to_fcombination(combination)
-board = bd.Board(combination)
+board = bd.Board(combination, players)
 hints = []  # type: List[Tuple[str, int]]
 simulations = []  # type: List[Tuple[str, Tuple[float, float]]]
 while True:
     choice = mn.display_main_menu(fcombination,
-                                  board.get_opponent_fcombinations(),
+                                  board.get_central_fcombinations(),
+                                  board.get_opponents_fcombinations(),
                                   hints,
                                   simulations)
     match choice:
         case 'h':
-            hint = mn.display_hints_menu()
+            hint = mn.display_hints_menu(players)
             if hint is not None:
-                num_opponent_combs_before = len(board.get_opponent_fcombinations())
-                board.apply_hint(hint[0], hint[1])
-                num_opponent_combs_after = len(board.get_opponent_fcombinations())
-                improvement = num_opponent_combs_before - num_opponent_combs_after
-                hints.append((hint, improvement))
+                hint_name = hint[0]
+                hint_results = []
+                num_opponent_combs_before = [len(opponent_combs) for opponent_combs in board.get_opponents_fcombinations()]
+                
+                for opponent, hint_result in enumerate(hint[1]):
+                    board.apply_hint(hint_name, hint_result, opponent)
+                
+                for opponent, hint_result in enumerate(hint[1]):
+                    num_opponent_combs_after = len(board.get_opponent_fcombinations(opponent))
+                    improvement = num_opponent_combs_before[opponent] - num_opponent_combs_after
+                    hint_results.append((hint_result, improvement))
+
+                hints.append((hint_name, hint_results))
                 simulations = []
         case 's':
             hints_to_simulate = mn.display_simulation_menu()
@@ -45,14 +55,21 @@ while True:
                     if hint not in [simulation[0] for simulation in simulations]:
                         simulations.append((hint, board.simulate(hint)))
         case 'c':
-            mn.display_combinations_menu(board.get_opponent_fcombinations())
+            opponent = -1
+            if players > 2:
+                opponent = int(input('Enter opponent number: '))
+            if opponent == -1:
+                mn.display_combinations_menu(board.get_central_fcombinations())
+            else: 
+                mn.display_combinations_menu(board.get_opponent_fcombinations(opponent))
         case 'u':
             if len(hints) > 0:
                 hints.pop()
                 simulations = []
-            board = bd.Board(combination)
+            board = bd.Board(combination, players)
             for hint in hints:
-                board.apply_hint(hint[0][0], hint[0][1])
+                for opponent, hint_result in enumerate(hint[1]):
+                    board.apply_hint(hint[0], hint_result[0], opponent)
         case 'q':
             really = input('Really quit? Press \'y\' to quit, anything else to go back: ')
             if really.lower() == 'y':
